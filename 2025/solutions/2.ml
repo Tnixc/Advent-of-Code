fdaca3aee813db4c2f4ffd4a36b560333e28a829
Tnixc <tnixxc@gmail.com>
2025-12-18 06:23:25 +0800

    justfile; slightly faster day2

--- 2025/solutions/2.ml
+++ 2025/solutions/2.ml
@@ -2,14 +2,33 @@ let input = String.trim {|
 #include "2"
 |} in
 
-let rec check2 str len span span_len =
-  if len = 0 then true
-  else if len < span_len || len mod span_len <> 0 then false
-  else if String.sub str 0 span_len <> span then false
+let twice n digits =
+  if digits mod 2 = 1 then false
   else
-    check2
-      (String.sub str span_len (len - span_len))
-      (len - span_len) span span_len
+    let half = digits / 2 in
+    let first = n / int_of_float (10. ** float_of_int half) in
+    let second = n mod int_of_float (10. ** float_of_int half) in
+    first = second
+in
+
+let repeats n digits =
+  let rec try_period p =
+    if p > digits / 2 then false
+    else if digits mod p <> 0 then try_period (p + 1)
+    else
+      let div = int_of_float (10. ** float_of_int (digits - p)) in
+      let pattern = n / div in
+      let rec check remaining len =
+        if len = 0 then true
+        else
+          let chunk_divisor = int_of_float (10. ** float_of_int (len - p)) in
+          let chunk = remaining / chunk_divisor in
+          if chunk <> pattern then false
+          else check (remaining mod chunk_divisor) (len - p)
+      in
+      if check n digits then true else try_period (p + 1)
+  in
+  try_period 1
 in
 
 let p1, p2 =
@@ -19,25 +38,16 @@ let p1, p2 =
       | [ a; b ] -> (int_of_string a, int_of_string b)
       | _ -> assert false)
   |> List.map (fun (a, b) ->
-      let rec range i (acc1, acc2) =
-        if i > b then (acc1, acc2)
-        else
-          let str = string_of_int i in
-          let len = String.length str in
-          if
-            len mod 2 = 0
-            && String.sub str 0 (len / 2) = String.sub str (len / 2) (len / 2)
-          then range (i + 1) (acc1 + i, acc2 + i)
-          else
-            let rec loop k count =
-              if k > len / 2 then count
-              else if check2 str len (String.sub str 0 k) k then count + i
-              else loop (k + 1) count
-            in
-            range (i + 1) (acc1, acc2 + loop 1 0)
-      in
-
-      range a (0, 0))
+      let p1 = ref 0 in
+      let p2 = ref 0 in
+      for i = a to b do
+        let digits = float_of_int i |> log10 |> ceil |> int_of_float in
+        if twice i digits then (
+          p1 := !p1 + i;
+          p2 := !p2 + i)
+        else if repeats i digits then p2 := !p2 + i
+      done;
+      (!p1, !p2))
   |> List.fold_left (fun (l1, l2) (r1, r2) -> (l1 + r1, l2 + r2)) (0, 0)
 in
 
